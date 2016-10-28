3f507f8a64bbdde8d71c5f6cb442afd5908375f2
Ryan Collins <rccproductions@mac.com>
2016-10-28 13:07:26 -0400

    Write action tests for create post container

--- client/app/src/containers/CreatePostContainer/tests/actions.test.js
+++ client/app/src/containers/CreatePostContainer/tests/actions.test.js
@@ -3,12 +3,57 @@ import * as actions from '../actions';
 import * as types from '../constants';
 
 describe('CreatePost actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
-      const expected = {
-        type: types.CREATEPOST_DEFAULT_ACTION,
-      };
-      expect(actions.createPostDefaultAction()).toEqual(expected);
-    });
+  it('should have a type of CREATE_POST_SET_SELECTED_TAGS', () => {
+    const tags = [
+      {
+        title: 'Yes amazing',
+      },
+      {
+        title: 'So cool',
+      },
+    ];
+    const expected = {
+      type: types.CREATE_POST_SET_SELECTED_TAGS,
+      tags,
+    };
+    expect(
+      actions.createPostSetSelectedTags(tags)
+    ).toEqual(expected);
+  });
+  it('should have a type of CREATE_POST_SET_MESSAGE', () => {
+    const message = 'Hello world';
+    const expected = {
+      type: types.CREATE_POST_SET_MESSAGE,
+      message,
+    };
+    expect(
+      actions.createPostSetMessage(message)
+    ).toEqual(expected);
+  });
+  it('should have a type of CREATE_POST_SET_ERROR', () => {
+    const error = new Error('OOps');
+    const expected = {
+      type: types.CREATE_POST_SET_MESSAGE,
+      error,
+    };
+    expect(
+      actions.createPostSetError(error)
+    ).toEqual(expected);
+  });
+  it('should have a type of CREATE_POST_CLEAR_MESSAGE', () => {
+    const expected = {
+      type: types.CREATE_POST_CLEAR_MESSAGE,
+    };
+    expect(
+      actions.createPostClearMessage()
+    ).toEqual(expected);
+  });
+  it('should have a type of CREATE_POST_CLEAR_ERROR', () => {
+    const expected = {
+      type: types.CREATE_POST_CLEAR_ERROR,
+    };
+    expect(
+      actions.createPostClearError()
+    ).toEqual(expected);
   });
 });
