5b45205b8611d209ae23d8c54d6d01fb79270acb
Ryan Collins <rccproductions@mac.com>
2016-10-28 12:48:15 -0400

    Feat: add reducer tests for the blog container

--- client/app/src/containers/BlogContainer/tests/reducer.test.js
+++ client/app/src/containers/BlogContainer/tests/reducer.test.js
@@ -1,4 +1,5 @@
 import expect from 'expect';
+import * as types from '../constants';
 import blogReducer, { initialState } from '../reducer';
 
 describe('blogReducer', () => {
@@ -6,5 +7,91 @@ describe('blogReducer', () => {
     expect(
       blogReducer(undefined, {})
     ).toEqual(initialState);
+  });
+  it('should handle reducer for BLOG_CLEAR_SEARCH_TERM', () => {
+    const stateBefore = {
+      searchTerm: 'Hi!',
+    };
+    const stateAfter = {
+      searchTerm: null,
+    };
+    expect(
+      blogReducer(stateBefore, {
+        type: types.BLOG_CLEAR_SEARCH_TERM,
+      })
+    ).toEqual(stateAfter);
+  });
+  it('should handle reducer for BLOG_SET_SEARCH_TERM', () => {
+    const searchTerm = 'Hi!';
+    const stateBefore = {
+      searchTerm: null,
+    };
+    const stateAfter = {
+      searchTerm,
+    };
+    expect(
+      blogReducer(stateBefore, {
+        type: types.BLOG_SET_SEARCH_TERM,
+        searchTerm,
+      })
+    ).toEqual(stateAfter);
+  });
+  it('should handle reducer for BLOG_ERROR', () => {
+    const error = new Error('An error has occured');
+    const stateBefore = {
+      error: null,
+    };
+    const stateAfter = {
+      error,
+    };
+    expect(
+      blogReducer(stateBefore, {
+        type: types.BLOG_ERROR,
+        error,
+      })
+    ).toEqual(stateAfter);
+  });
+  it('should handle reducer for BLOG_MESSAGE', () => {
+    const message = 'It worked!';
+    const stateBefore = {
+      message: null,
+    };
+    const stateAfter = {
+      message,
+    };
+    expect(
+      blogReducer(stateBefore, {
+        type: types.BLOG_MESSAGE,
+        message,
+      })
+    ).toEqual(stateAfter);
+  });
+  it('should handle reducer for CLEAR_BLOG_ERROR', () => {
+    const error = new Error('An error has occured');
+    const stateBefore = {
+      error,
+    };
+    const stateAfter = {
+      error: null,
+    };
+    expect(
+      blogReducer(stateBefore, {
+        type: types.CLEAR_BLOG_ERROR,
+      })
+    ).toEqual(stateAfter);
+  });
+  it('should handle reducer for CLEAR_BLOG_MESSAGE', () => {
+    const message = 'It worked!';
+    const stateBefore = {
+      message,
+    };
+    const stateAfter = {
+      message: null,
+    };
+    expect(
+      blogReducer(stateBefore, {
+        type: types.CLEAR_BLOG_MESSAGE,
+      })
+    ).toEqual(stateAfter);
   });
 });
