ced7abf75dbf01f6b0b90c3d323099a7db3028bd
Ryan Collins <rccproductions@mac.com>
2016-10-29 07:20:36 -0400

    Fix: import techstack vs techStack in landing container

--- client/app/src/containers/LandingContainer/data/index.js
+++ client/app/src/containers/LandingContainer/data/index.js
@@ -2,4 +2,4 @@ export milestones from './milestones';
 export summary from './summary';
 export chartData from './chartData';
 export languages from './languages';
-export techStack from './techStack';
+export techstack from './techstack';
