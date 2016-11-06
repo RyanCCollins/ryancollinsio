1714082f2d181460f8daa73e3bd126cf483e722f
Ryan Collins <rccproductions@mac.com>
2016-11-06 16:55:55 -0500

    feat: update the project comment migration

--- app/models/project_comment_vote.rb
+++ app/models/project_comment_vote.rb
@@ -1,4 +1,4 @@
 class ProjectCommentVote < ApplicationRecord
-  belongs_to :project
   belongs_to :user
+  belongs_to :project_comment
 end
--- db/migrate/20161106214812_create_project_comment_votes.rb
+++ db/migrate/20161106214812_create_project_comment_votes.rb
@@ -1,7 +1,7 @@
 class CreateProjectCommentVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :project_comment_votes do |t|
-      t.references :project, foreign_key: true
+      t.references :project_comment, foreign_key: true
       t.references :user, foreign_key: true
 
       t.timestamps
