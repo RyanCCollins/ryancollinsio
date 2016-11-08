f3ecb9b5982445fd98d965273fd2e52dc75a52ec
Ryan Collins <rccproductions@mac.com>
2016-11-08 11:50:26 -0500

    feat: Add context with insert css to index.js

--- client/app/src/index.js
+++ client/app/src/index.js
@@ -11,12 +11,22 @@ import { install } from 'offline-plugin/runtime';
 const isProduction = process.env.NODE_ENV === 'production';
 const node = document.getElementById('app');
 
+const context = {
+  // Enables critical path CSS rendering
+  // https://github.com/kriasoft/isomorphic-style-loader
+  insertCss: (...styles) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const removeCss = styles.map(x => x._insertCss());
+    return () => { removeCss.forEach(f => f()); };
+  },
+};
+
 match({ history, routes },
   (error, redirectLocation, renderProps) => { // eslint-disable-line
     if (error) {
       return console.error('Require.ensure error'); // eslint-disable-line
     }
-    render(<RouterApp {...renderProps} />, node);
+    render(<RouterApp {...renderProps} context={context} />, node);
   });
 
 if (isProduction) {
