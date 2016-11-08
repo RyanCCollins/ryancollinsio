b6e42c5a0ae17a461557a1626b7f2584ebfb6b80
Ryan Collins <rccproductions@mac.com>
2016-11-08 10:45:53 -0500

    feat: Begin working on the mobile implementation

---
+++ mobile/apolloClient.js
@@ -0,0 +1,17 @@
+import ApolloClient, {
+  createNetworkInterface,
+  addTypeName,
+} from 'apollo-client';
+const isProduction = true;
+const baseUrl = isProduction ?
+  'https://ryancollinsio.herokuapp.com/graphql' : 'http://localhost:3000/graphql';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: baseUrl
+  }),
+  initialState: typeof window !== 'undefined' ? window.__APOLLO_STATE__ : null,
+  queryTransformer: addTypeName,
+});
+
+export default client;
--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -3,51 +3,123 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import { ApolloProvider } from 'react-apollo';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import client from './apolloClient';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  Image,
+  ActivityIndicator,
+  ListViewDataSource
 } from 'react-native';
 
-export default class mobile extends Component {
+const Row = ({ title, featureImage, caption, description }) => (
+  <View style={styles.rowWrapper}>
+    <Image
+      source={{ uri: featureImage }}
+    />
+    <View stlye={styles.column}>
+      <Text>{title}</Text>
+      <Text>{caption}</Text>
+      <Text>{description}</Text>
+    </View>
+  </View>
+);
+
+export default class MainContainer extends Component {
+  constructor() {
+    super();
+    this.handleListViewDataSource = this.handleListViewDataSource.bind(this);
+  }
+  componentWillReceiveProps({ allProjects }) {
+    if (allProjects) {
+      console.log(`Got all projects ${allProjects}`)
+    }
+  }
+  handleListViewDataSource() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    return {
+      dataSource: ds.cloneWithRows(this.props.allProjects)
+    }
+  }
   render() {
+    const {
+      allProjects,
+      isLoading,
+    } = this.props;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>RyanCollins.io</Text>
+        {/* {isLoading || !allProjects ?
+          <ActivityIndicator
+            animating={true}
+          />
+        :
+          <ListView
+            dataSource={this.handleListViewDataSource()}
+            renderRow={(item) => <Row {...item} />}
+          />
+        } */}
       </View>
     );
   }
 }
 
+const getProjectsQuery = gql`
+  query loadProjects {
+    projects(status: "published") {
+      title
+      status
+      description
+      user {
+        name
+      }
+      slug
+      caption
+      featureImage
+      tags {
+        title
+      }
+    }
+  }
+`;
+
+const ContainerWithData = graphql(getProjectsQuery, {
+  props: ({ data: { projects, loading, error } }) => ({
+    allProjects: projects,
+    isLoading: loading,
+    loadingError: error,
+  }),
+})(MainContainer);
+
+
 const styles = StyleSheet.create({
+  rowWrapper: {
+    flexDirection: 'row',
+  },
+  column: {
+    flexDirection: 'column',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-AppRegistry.registerComponent('mobile', () => mobile);
+const App = () => (
+  <ApolloProvider client={client}>
+    <ContainerWithData />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent('mobile', () => App);
