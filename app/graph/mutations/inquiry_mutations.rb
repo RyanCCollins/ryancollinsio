46e2a800fba9c0b0d00c4fb2ff7c4d97a4b64953
Ryan Collins <rccproductions@mac.com>
2016-11-14 12:42:56 -0500

    fix: Add underscore to inquiry mutations ctx

--- app/graph/mutations/inquiry_mutations.rb
+++ app/graph/mutations/inquiry_mutations.rb
@@ -5,7 +5,7 @@ module InquiryMutations
     input_field :inquiry, InquiryInputType
     return_field :id, types.ID
 
-    resolve -> (inputs, ctx) do
+    resolve -> (inputs, _ctx) do
       parsed_inputs = inputs[:inquiry]
       inquiry = Inquiry.create(
         name: parsed_inputs[:name],
