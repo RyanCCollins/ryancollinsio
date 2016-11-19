3d8316e74b2e66ed8e1cd5bc978e34a627ed5acb
Ryan Collins <techrapportrc@gmail.com>
2016-11-19 09:02:42 +0000

    Feat: set the searchbar to go left on mobile

--- client/app/src/components/Navbar/index.js
+++ client/app/src/components/Navbar/index.js
@@ -31,7 +31,7 @@ const Navbar = ({
         <StyledBox>
           <Search
             onDOMChange={onSearch}
-            dropAlign={{ left: 'left' }}
+            dropAlign={{ right: 'right' }}
             placeHolder="Start typing..."
           />
         </StyledBox>
