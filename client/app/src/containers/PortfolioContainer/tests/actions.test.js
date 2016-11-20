797dc38cadb7e940c30207e67f62e70667113ad0
Ryan Collins <techrapportrc@gmail.com>
2016-11-20 01:09:49 +0000

    Feat: Add multiple tests to containers

--- client/app/src/containers/PortfolioContainer/tests/actions.test.js
+++ client/app/src/containers/PortfolioContainer/tests/actions.test.js
@@ -3,14 +3,50 @@ import * as actions from '../actions';
 import * as types from '../constants';
 
 describe('Portfolio actions', () => {
-  describe('Default Action', () => {
-    it('has a type of PORTFOLIO_SET_PROJECTS', () => {
-      const projects = [];
-      const expected = {
-        type: types.PORTFOLIO_SET_PROJECTS,
-        projects,
-      };
-      expect(actions.portfolioSetProjects(projects)).toEqual(expected);
-    });
+  it('should have a type of PORTFOLIO_SET_PROJECTS', () => {
+    const projects = [];
+    const expected = {
+      type: types.PORTFOLIO_SET_PROJECTS,
+      projects,
+    };
+    expect(actions.portfolioSetProjects(projects)).toEqual(expected);
+  });
+  it('should have a type of PORTFOLIO_SET_CURRENT_PAGE', () => {
+    const page = 2;
+    const expected = {
+      type: types.PORTFOLIO_SET_CURRENT_PAGE,
+      page,
+    };
+    expect(
+      actions.portfolioSetCurrentPage(page)
+    ).toEqual(expected);
+  });
+  it('should have a type of PORTFOLIO_SET_SEARCH_TERM', () => {
+    const searchTerm = 'Foobar';
+    const expected = {
+      type: types.PORTFOLIO_SET_SEARCH_TERM,
+      searchTerm,
+    };
+    expect(
+      actions.portfolioSetSearchTerm(searchTerm)
+    ).toEqual(expected);
+  });
+  it('should have a type of PORTFOLIO_CLEAR_SEARCH_TERM', () => {
+    const expected = {
+      type: types.PORTFOLIO_CLEAR_SEARCH_TERM,
+    };
+    expect(
+      actions.portfolioClearSearchTerm()  
+    ).toEqual(expected);
+  });
+  it('should have a type of PORTFOLIO_SET_TAGS', () => {
+    const tags = [{}, {}];
+    const expected = {
+      type: types.PORTFOLIO_SET_TAGS,
+      tags,
+    };
+    expect(
+      actions.portfolioSetTags(tags)  
+    ).toEqual(expected);
   });
 });
--- client/app/src/containers/PortfolioContainer/tests/reducer.test.js
+++ client/app/src/containers/PortfolioContainer/tests/reducer.test.js
@@ -27,4 +27,68 @@ describe('portfolioReducer', () => {
       })
     ).toEqual(stateAfter);
   });
+  it('should handle reducer for PORTFOLIO_SET_CURRENT_PAGE', () => {
+    const page = 2;
+    const stateBefore = {
+      currentPage: 1,
+    };
+    const stateAfter = {
+      currentPage: page,
+    };
+    expect(
+      portfolioReducer(stateBefore, {
+        type: types.PORTFOLIO_SET_CURRENT_PAGE,
+        page,
+      })  
+    ).toEqual(stateAfter);
+  });
+  it('should handle reducer for PORTFOLIO_SET_SEARCH_TERM', () => {
+    const searchTerm = 'fazzbuzz';
+    const stateBefore = {
+      searchTerm: null,
+      isFiltering: false,
+    };
+    const stateAfter = {
+      searchTerm,
+      isFiltering: true,
+    };
+    expect(
+      portfolioReducer(stateBefore, {
+        type: types.PORTFOLIO_SET_SEARCH_TERM,
+        searchTerm,
+      })  
+    ).toEqual(stateAfter);   
+  });
+  it('should handle reducer for PORTFOLIO_CLEAR_SEARCH_TERM', () => {
+    const stateBefore = {
+      searchTerm: 'Foobar',
+      isFiltering: true,
+    };
+    const stateAfter = {
+      searchTerm: null,
+      isFiltering: false,
+    };
+    expect(
+      portfolioReducer(stateBefore, {
+        type: types.PORTFOLIO_CLEAR_SEARCH_TERM,
+      })  
+    ).toEqual(stateAfter);  
+  });
+  it('should handle reducer for PORTFOLIO_SET_TAGS', () => {
+    const tags = [{ id: 0 }, { id: 1 }];
+    const stateBefore = {
+      tags: [],
+      isFiltering: false,
+    };
+    const stateAfter = {
+      tags,
+      isFiltering: true,
+    };
+    expect(
+      portfolioReducer(stateBefore, {
+        type: types.PORTFOLIO_SET_TAGS,
+        tags,
+      })  
+    ).toEqual(stateAfter);  
+  });
 });
