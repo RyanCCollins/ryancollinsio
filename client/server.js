50d9fa1a94666f3bfd0a04797b7beea6f0ccd2f6
Ryan Collins <techrapportrc@gmail.com>
2016-11-24 23:37:52 +0000

    Feat: Update the server to no longer run css module hook

--- client/server.js
+++ client/server.js
@@ -1,8 +1,8 @@
-var hook = require('css-modules-require-hook');
-hook({
-  generateScopedName: '[path]___[name]__[local]___[hash:base64:5]',
-  extensions: ['.scss']
-});
+// var hook = require('css-modules-require-hook');
+// hook({
+//   generateScopedName: '[path]___[name]__[local]___[hash:base64:5]',
+//   extensions: ['.scss']
+// });
 require('babel-core/register');
 require('./config/webpack/ignoreAssets');
 var app = require('./server/app');
