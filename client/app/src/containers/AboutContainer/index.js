d7b39bc5f5c96bd7057c7aacb7a509ad69ffbc3e
Ryan Collins <rccproductions@mac.com>
2016-12-01 10:49:47 -0500

    Feat: add loading indicator to about container

--- client/app/src/containers/AboutContainer/index.js
+++ client/app/src/containers/AboutContainer/index.js
@@ -7,24 +7,26 @@ import LinkPreviousIcon from 'grommet-udacity/components/icons/base/LinkPrevious
 import Markdown from 'grommet-udacity/components/Markdown';
 import Heading from 'grommet-udacity/components/Heading';
 import about from './index.md';
-import { Divider } from 'components';
+import { Divider, WithLoading } from 'components';
 import { StyledBox, AvatarImage, StyledArticle, BoxWrapper } from './styles';
 
 const AboutContainer = ({ history }) => (
   <StyledBox pad="large">
     <Section primary align="center" justify="center" pad="large">
-      <StyledArticle align="center">
-        <Headline className="heading" align="center">
-          About RyanCollins.io
-        </Headline>
-        <Divider />
-        {typeof about === 'string' &&
-          <Markdown
-            align="center"
-            content={about}
-          />
-        }
-      </StyledArticle>
+      <WithLoading isLoading={typeof about !== 'string'}>
+        <StyledArticle align="center">
+          <Headline className="heading" align="center">
+            About RyanCollins.io
+          </Headline>
+          <Divider />
+          {typeof about === 'string' &&
+            <Markdown
+              align="center"
+              content={about}
+            />
+          }
+        </StyledArticle>
+      </WithLoading>
     </Section>
     <Section primary align="center" justify="center" pad="large">
       <StyledArticle align="center">
