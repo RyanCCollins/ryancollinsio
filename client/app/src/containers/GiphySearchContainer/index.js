169b23360bdce2b846df89dd83b8fec6d041fda7
Ryan Collins <rccproductions@mac.com>
2016-12-18 12:23:35 -0500

    Feat: update giphy search container elm import

--- client/app/src/containers/GiphySearchContainer/index.js
+++ client/app/src/containers/GiphySearchContainer/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Elm from 'react-elm-components';
 import Heading from 'grommet-udacity/components/Heading';
 import Section from 'grommet-udacity/components/Section';
 import Markdown from 'grommet-udacity/components/Markdown';
 import Box from 'grommet-udacity/components/Box';
-import { StyledWrapper, StyledArticle } from './styles';
-import { Main } from 'elm/Main';
+import { Main } from 'elm/Main.elm'; // eslint-disable-line
 import { Divider } from 'components';
+import { StyledWrapper, StyledArticle } from './styles';
 
 const markdownContent = `### Congrats on finding the easter egg!
   \n I have put together a Giphy Search app for your amusement.
@@ -14,31 +14,23 @@ const markdownContent = `### Congrats on finding the easter egg!
   [article on the subject](https://medium.com/@ryancollinsio/embedding-elm-into-a-react-app-41e0233e942#.zib8d55g7).
   \n \n Maybe I will make a game next.  Have any good ideas?  \n \n Anyways, enjoy!`;
 
-class GiphySearchContainer extends Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <StyledWrapper pad="large" className="gradient-blue">
-        <Section justify="center" align="center">
-          <StyledArticle align="center" pad="large">
-            <Heading tag="h1" align="center">
-              Elm Giphy Search
-            </Heading>
-            <Divider />
-            <Box pad="large" align="center">
-              <Markdown content={markdownContent} />
-            </Box>
-          </StyledArticle>
-        </Section>
-        <Section>
-          <Elm src={Main} />
-        </Section>
-      </StyledWrapper>
-    );
-  }
-}
-
-GiphySearchContainer.propTypes = {
-  // isLoading: PropTypes.bool.isRequired,
-};
+const GiphySearchContainer = () => (
+  <StyledWrapper pad="large" className="gradient-blue">
+    <Section justify="center" align="center">
+      <StyledArticle align="center" pad="large">
+        <Heading tag="h1" align="center">
+          Elm Giphy Search
+        </Heading>
+        <Divider />
+        <Box pad="large" align="center">
+          <Markdown content={markdownContent} />
+        </Box>
+      </StyledArticle>
+    </Section>
+    <Section>
+      <Elm src={Main} />
+    </Section>
+  </StyledWrapper>
+);
 
 export default GiphySearchContainer;
