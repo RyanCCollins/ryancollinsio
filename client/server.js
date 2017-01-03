702dc6abe974f8e7abce6e4a8c63d1a42fb3273b
Ryan Collins <rccproductions@mac.com>
2017-01-02 23:05:53 -0500

    Fix: webpack version, removing caret

--- client/server.js
+++ client/server.js
@@ -1,4 +1,4 @@
 require('babel-polyfill');
 require('babel-core/register');
 require('./config/webpack/ignoreAssets');
-const app = require('./server/app');
+const app = require('./server/app'); // eslint-disable-line
