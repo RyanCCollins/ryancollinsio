49a67cd5f99fb04f3df3d582c1fcb2c13be45c69
Ryan Collins <rccproductions@mac.com>
2017-01-03 18:40:13 -0500

    Fix: adjust cubic bezier for logo image

--- client/app/src/components/LogoImage/index.js
+++ client/app/src/components/LogoImage/index.js
@@ -3,7 +3,7 @@ import Logo from './styles';
 
 const LogoImage = () => (
   <Logo
-    animation="fadeInFromRight 2s linear 0.5s"
+    animation="fadeInFromRight 1.5s ease-out 0.2s"
     url="https://github.com/RyanCCollins/cdn/blob/master/ryancollinsio-v3/crown.png?raw=true"
   />
 );
