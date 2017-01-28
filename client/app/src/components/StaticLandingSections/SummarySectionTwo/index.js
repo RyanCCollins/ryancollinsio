071f5e0f30d3c63b8cf4a320a997ba742db2026e
Ryan Collins <rccproductions@mac.com>
2017-01-28 18:23:18 -0500

    Fix: remove not working animate component

--- client/app/src/components/StaticLandingSections/SummarySectionTwo/index.js
+++ client/app/src/components/StaticLandingSections/SummarySectionTwo/index.js
@@ -27,66 +27,48 @@ const SummarySectionTwo = () => (
     </Headline>
     <Divider />
     <Logos>
-      <Animate
-        keep
-        visible="scroll"
-        enter={{ animation: 'slide-up', duration: 1000, delay: 0 }}
-      >
-        <Card>
-          <CardLink href="https://github.com/udacityalumni">
-            <CardTop>
-              <Logo>
-                <UdacityLogo />
-              </Logo>
-            </CardTop>
-            <CardBottom>
-              <ProjectName align="center" tag="h2">
-                Udacity Alumni
-              </ProjectName>
-            </CardBottom>
-          </CardLink>
-        </Card>
-      </Animate>
-      <Animate
-        keep
-        visible="scroll"
-        enter={{ animation: 'slide-up', duration: 1000, delay: 1000 }}
-      >
-        <Card>
-          <CardLink href="https://github.com/grommet/grommet">
-            <CardTop>
-              <Logo>
-                <GrommetIcon size="xlarge" colorIndex="brand" />
-              </Logo>
-            </CardTop>
-            <CardBottom>
-              <ProjectName tag="h2">
-                Grommet
-              </ProjectName>
-            </CardBottom>
-          </CardLink>
-        </Card>
-      </Animate>
-      <Animate
-        keep
-        visible="scroll"
-        enter={{ animation: 'slide-up', duration: 1000, delay: 2000 }}
-      >
-        <Card>
-          <CardLink href="https://github.com/udacity-blitz">
-            <CardTop>
-              <Logo>
-                <BlitzLogo />
-              </Logo>
-            </CardTop>
-            <CardBottom>
-              <ProjectName tag="h2">
-                Udacity Blitz
-              </ProjectName>
-            </CardBottom>
-          </CardLink>
-        </Card>
-      </Animate>
+      <Card>
+        <CardLink href="https://github.com/udacityalumni">
+          <CardTop>
+            <Logo>
+              <UdacityLogo />
+            </Logo>
+          </CardTop>
+          <CardBottom>
+            <ProjectName align="center" tag="h2">
+              Udacity Alumni
+            </ProjectName>
+          </CardBottom>
+        </CardLink>
+      </Card>
+      <Card>
+        <CardLink href="https://github.com/grommet/grommet">
+          <CardTop>
+            <Logo>
+              <GrommetIcon size="xlarge" colorIndex="brand" />
+            </Logo>
+          </CardTop>
+          <CardBottom>
+            <ProjectName tag="h2">
+              Grommet
+            </ProjectName>
+          </CardBottom>
+        </CardLink>
+      </Card>
+      <Card>
+        <CardLink href="https://github.com/udacity-blitz">
+          <CardTop>
+            <Logo>
+              <BlitzLogo />
+            </Logo>
+          </CardTop>
+          <CardBottom>
+            <ProjectName tag="h2">
+              Udacity Blitz
+            </ProjectName>
+          </CardBottom>
+        </CardLink>
+      </Card>
     </Logos>
   </Section>
 );
