d9730ec3fcb36fb072ce5a536bf5c393985d5ba4
Ryan Collins <rccproductions@mac.com>
2017-10-21 14:14:44 -0400

    Add recompose to TutorialContainer

--- client/app/src/containers/TutorialContainer/index.js
+++ client/app/src/containers/TutorialContainer/index.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import cssModules from 'react-css-modules';
@@ -7,7 +7,7 @@ import Section from 'grommet-udacity/components/Section';
 import Headline from 'grommet-udacity/components/Headline';
 import Paragraph from 'grommet-udacity/components/Paragraph';
 import Markdown from 'grommet-udacity/components/Markdown';
-import { compose } from 'recompose';
+import { compose, lifecycle, withHandlers } from 'recompose';
 import { Divider, WithLoading, CommentFeed, WithToast } from 'components';
 import withData from './gql';
 import * as TutorialActionCreators from './actions';
@@ -18,149 +18,72 @@ if (typeof window !== 'undefined') {
   RichTextEditor = require('react-rte').default; // eslint-disable-line
 }
 
-class TutorialContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUpvote = this.handleUpvote.bind(this);
-    this.checkAuthToken = this.checkAuthToken.bind(this);
-    this.handleResettingComment = this.handleResettingComment.bind(this);
-  }
-  componentDidMount() {
-    this.handleResettingComment();
-  }
-  handleResettingComment() {
-    if (RichTextEditor) {
-      this.props.actions.tutorialEditComment(RichTextEditor.createEmptyValue());
-    }
-  }
-  handleUpvote(id) {
-    const {
-      upvoteComment,
-      refetch,
-      user,
-    } = this.props;
-    if (!user || !user.authToken) {
-      this.checkAuthToken();
-    } else {
-      const data = {
-        variables: {
-          authToken: user.authToken,
-          id: parseInt(id, 10),
-        },
-      };
-      upvoteComment(data).then(() => {
-        refetch();
-      }).catch((err) => {
-        this.props.actions.tutorialError(err);
-      });
-    }
-  }
-  handleSubmit() {
-    const {
-      mutate,
-      refetch,
-      user,
-      commentInput,
-    } = this.props;
-    if (!user || !user.authToken) {
-      this.checkAuthToken();
-    } else {
-      const data = {
-        variables: {
-          authToken: user.authToken,
-          comment: {
-            body: commentInput.toString('markdown'),
-          },
-          id: parseInt(this.props.tutorial.id, 10),
-        },
-      };
-      mutate(data).then(() => {
-        refetch();
-        this.props.actions.tutorialMessage('Comment successfully submitted!');
-        this.handleResettingComment();
-      }).catch((err) => {
-        this.props.actions.tutorialError(err);
-      });
-    }
-  }
-  checkAuthToken() {
-    const {
-      user,
-    } = this.props;
-    if (!user || !user.authToken) {
-      this.context.router.push('/login');
-    }
-  }
-  render() {
-    const {
-      isLoading,
-      tutorial,
-      commentInput,
-      actions,
-      commentError,
-      message,
-      user,
-    } = this.props;
-    return (
-      <WithLoading fullscreen isLoading={isLoading}>
-        <WithToast
-          error={commentError}
-          message={message}
-          onClose={actions.tutorialClearToast}
-        >
-          <Section
-            primary
-            className={styles.portfolio}
-            colorIndex="light-2"
-            align="center"
-            justify="center"
-            pad="large"
-          >
-            <Headline className="heading" align="center">
-              {tutorial && tutorial.title}
-            </Headline>
-            <Divider />
-          </Section>
-          {tutorial &&
-            <Box align="center" justify="center" colorIndex="light-2">
-              <iframe
-                width="960"
-                height="480"
-                src={`https://www.youtube.com/embed/${tutorial.link.split('/')[3]}`}
-                frameBorder="0"
-                allowFullScreen
-              />
-            </Box>
-          }
-          <Section align="center" colorIndex="light-2">
-            <Paragraph>
-              {tutorial && tutorial.description}
-            </Paragraph>
-            <Box align="center" pad="medium" className="main-text markdown-body">
-              <Markdown content={tutorial && tutorial.body} />
-            </Box>
-          </Section>
-          <CommentFeed
-            value={commentInput}
-            onChange={value => actions.tutorialEditComment(value)}
-            onSubmit={this.handleSubmit}
-            comments={tutorial && tutorial.comments}
-            onUpvote={this.handleUpvote}
-            user={user}
+const TutorialContainer = ({
+  isLoading,
+  tutorial,
+  commentInput,
+  actions,
+  commentError,
+  message,
+  user,
+  handleSubmit,
+  handleUpvote,
+}) => (
+  <WithLoading fullscreen isLoading={isLoading}>
+    <WithToast
+      error={commentError}
+      message={message}
+      onClose={actions.tutorialClearToast}
+    >
+      <Section
+        primary
+        className={styles.portfolio}
+        colorIndex="light-2"
+        align="center"
+        justify="center"
+        pad="large"
+      >
+        <Headline className="heading" align="center">
+          {tutorial && tutorial.title}
+        </Headline>
+        <Divider />
+      </Section>
+      {tutorial &&
+        <Box align="center" justify="center" colorIndex="light-2">
+          <iframe
+            width="960"
+            height="480"
+            src={`https://www.youtube.com/embed/${tutorial.link.split('/')[3]}`}
+            frameBorder="0"
+            allowFullScreen
           />
-        </WithToast>
-      </WithLoading>
-    );
-  }
-}
+        </Box>
+      }
+      <Section align="center" colorIndex="light-2">
+        <Paragraph>
+          {tutorial && tutorial.description}
+        </Paragraph>
+        <Box align="center" pad="medium" className="main-text markdown-body">
+          <Markdown content={tutorial && tutorial.body} />
+        </Box>
+      </Section>
+      <CommentFeed
+        value={commentInput}
+        onChange={value => actions.tutorialEditComment(value)}
+        onSubmit={handleSubmit}
+        comments={tutorial && tutorial.comments}
+        onUpvote={handleUpvote}
+        user={user}
+      />
+    </WithToast>
+  </WithLoading>
+);
 
 TutorialContainer.propTypes = {
   tutorial: PropTypes.object,
   isLoading: PropTypes.bool,
-  upvoteComment: PropTypes.func,
-  mutate: PropTypes.func,
-  refetch: PropTypes.func,
+  handleUpvote: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   commentInput: PropTypes.object,
   actions: PropTypes.object.isRequired,
@@ -196,4 +119,70 @@ export default compose(
     mapDispatchToProps,
   ),
   withData,
+  withHandlers({
+    checkAuthToken: ({ user }) => () => {
+      if (!user || !user.authToken) {
+        this.context.router.push('/login');
+      }
+    },
+    handleUpvote: ({ upvoteComment, refetch, user, actions }) => (id) => {
+      if (!user || !user.authToken) {
+        this.checkAuthToken();
+      } else {
+        const data = {
+          variables: {
+            authToken: user.authToken,
+            id: parseInt(id, 10),
+          },
+        };
+        upvoteComment(data)
+          .then(() => {
+            refetch();
+          })
+          .catch((err) => {
+            actions.tutorialError(err);
+          });
+      }
+    },
+    handleSubmit: ({
+      mutate,
+      refetch,
+      user,
+      commentInput,
+      checkAuthToken,
+      actions,
+      tutorial,
+    }) => () => {
+      if (!user || !user.authToken) {
+        checkAuthToken();
+      } else {
+        const data = {
+          variables: {
+            authToken: user.authToken,
+            comment: {
+              body: commentInput.toString('markdown'),
+            },
+            id: parseInt(tutorial.id, 10),
+          },
+        };
+        mutate(data).then(() => {
+          refetch();
+          actions.tutorialMessage('Comment successfully submitted!');
+          if (RichTextEditor) {
+            actions.tutorialEditComment(RichTextEditor.createEmptyValue());
+          }
+        }).catch((err) => {
+          actions.tutorialError(err);
+        });
+      }
+    },
+  }),
+  lifecycle({
+    componentDidMount() {
+      const { actions } = this.props;
+      if (RichTextEditor) {
+        actions.tutorialEditComment(RichTextEditor.createEmptyValue());
+      }
+    },
+  }),
 )(Container);
